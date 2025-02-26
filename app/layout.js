3cabcb70979dd1d0ee6281dd1e4ca10ae6ac914d
DevMaverickMB <milindbhushan007@gmail.com>
2025-02-27 00:09:25 +0530

    updated theme

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
         <body className={`${inter.className}`}>
           <ThemeProvider
             attribute="class"
-            // defaultTheme="dark"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
